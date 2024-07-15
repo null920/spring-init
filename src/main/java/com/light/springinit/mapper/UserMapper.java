954b70b1f07ee140639e0b7f3d20f7e4bc3ecccf
null920 <3590543392@qq.com>
2024-07-15 23:04:14 +0800

    first commit

---
+++ src/main/java/com/light/springinit/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.light.springinit.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.light.springinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
